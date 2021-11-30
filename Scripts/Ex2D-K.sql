51afe4acbf7725eb8718f196733f07d6ec6cc5f9
JLuzioS <63779047+JLuzioS@users.noreply.github.com>
2021-11-30 12:03:14 +0000

    feat: add delete and insert Funcionario

--- Scripts/Ex2D-K.sql
+++ Scripts/Ex2D-K.sql
@@ -1,2 +1,31 @@
-CREATE PROCEDURE insertFuncionario
-AS 
+CREATE PROCEDURE insertFuncionario  @cc VARCHAR(13), 
+                                    @nif VARCHAR(12), 
+                                    @nome_completo VARCHAR(256), 
+                                    @data_de_nascimento DATE, 
+                                    @morada VARCHAR(256), 
+                                    @codigo_postal VARCHAR(8), 
+                                    @localidade VARCHAR(256), 
+                                    @profissao VARCHAR(256), 
+                                    @telefone INT, 
+                                    @telemovel INT
+AS
+    BEGIN
+        DECLARE @id AS BIGINT
+        SELECT TOP 1 @id = id + 1  FROM Funcionarios ORDER BY id ASC
+        INSERT INTO Funcionarios VALUES (ISNULL(@id, 1), @cc, @nif, @nome_completo, @data_de_nascimento, @morada, @codigo_postal, @localidade, @profissao, @telefone, @telemovel)
+    END
+GO
+
+
+
+CREATE PROCEDURE deleteFuncionario  @id INT
+AS
+    BEGIN
+        if @id IS NOT NULL
+        BEGIN
+            DELETE FROM Funcionarios where id = @id
+        END
+    END
+GO
+
+
