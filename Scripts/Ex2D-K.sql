615879cd4eee8c3d29844e37e36187bbd82f6198
Tiago Silva <tiagosik1994@gmail.com>
2021-11-30 12:04:16 +0000

    feat: add update Funcionario

--- Scripts/Ex2D-K.sql
+++ Scripts/Ex2D-K.sql
@@ -14,9 +14,51 @@ AS
         SELECT TOP 1 @id = id + 1  FROM Funcionarios ORDER BY id ASC
         INSERT INTO Funcionarios VALUES (ISNULL(@id, 1), @cc, @nif, @nome_completo, @data_de_nascimento, @morada, @codigo_postal, @localidade, @profissao, @telefone, @telemovel)
     END
+    
 GO
 
+CREATE PROCEDURE updateFuncionario (
+    @id INT,
+    @cc VARCHAR(13) = NULL,
+    @nif VARCHAR(12) = NULL,
+    @nome_completo VARCHAR(256) = NULL,
+    @data_de_nascimento DATE = NULL,
+    @morada VARCHAR(256) = NULL,
+    @codigo_postal VARCHAR(8) = NULL,
+    @localidade VARCHAR(256) = NULL,
+    @profissao VARCHAR(256) = NULL,
+    @telefone INT = NULL,
+    @telemovel INT = NULL)
+AS   
+BEGIN
+    IF (NULLIF(@id, '') IS NULL)
+        RAISERROR ('Funcionario ID can''t be null', 10, 0)
+    SET @cc = NULLIF(@cc, '')
+    SET @nif = NULLIF(@nif, '')
+    SET @nome_completo = NULLIF(@nome_completo, '') 
+    SET @data_de_nascimento = NULLIF(@data_de_nascimento, '') 
+    SET @morada = NULLIF(@morada, '')
+    SET @codigo_postal = NULLIF(@codigo_postal, '')
+    SET @localidade = NULLIF(@localidade, '')
+    SET @profissao = NULLIF(@profissao, '') 
+    SET @telefone = NULLIF(@telefone, '')
+    SET @telemovel = NULLIF(@telemovel, '') 
 
+    UPDATE Funcionarios 
+	SET cc = ISNULL(@cc, cc), 
+		nif = ISNULL(@nif, nif),
+		nome_completo = ISNULL(@nome_completo, nome_completo),
+		data_de_nascimento = ISNULL(@data_de_nascimento, data_de_nascimento),
+		morada = ISNULL(@morada, morada),
+		codigo_postal = ISNULL(@codigo_postal, codigo_postal),
+		localidade = ISNULL(@localidade, localidade),
+		profissao = ISNULL(@profissao, profissao), 
+		telefone = ISNULL(@telefone, telefone),
+		telemovel = ISNULL(@telemovel, telemovel) 
+    WHERE id = @id;
+END;
+
+GO
 
 CREATE PROCEDURE deleteFuncionario  @id INT
 AS
