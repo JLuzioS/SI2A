fec44ad0bc6d44d7035133e77b486272a712bf0c
peufemia <paulo_eufemia@sapo.pt>
2022-01-08 02:54:21 +0000

    Create TrabSI2

---
+++ TrabSI2/ModelLayer/PrecosActivos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    public partial class PrecosActivos
+    {
+        public int activo { get; set; }
+        public System.DateTime dtAtualizacao { get; set; }
+        public decimal preco { get; set; }
+
+        public virtual Activos Activos { get; set; }
+    }
+}
---
+++ TrabSI2/ModelLayer/TiposActivos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    public partial class TiposActivos
+    {
+        public TiposActivos()
+        {
+            this.Activos = new HashSet<Activos>();
+        }
+
+        public int id { get; set; }
+        public string descricao { get; set; }
+
+        public virtual ICollection<Activos> Activos { get; set; }
+    }
+}
